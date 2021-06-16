9e309a8f389f355a0589c4be4b00450b307c8106
aym37 <aymen.khennache@dataimpact.io>
2021-06-16 17:37:04 +0100

    T800 AVD: deleted useless files

--- t800_avd/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir ./android/frida
-wget 'https://github.com/hluwa/strongR-frida-android/releases/download/14.2.18/hluda-server-14.2.18-android-x86' -P ./android/frida/
-mkdir ./android/SDK
-wget 'https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip' -O ./android/SDK/sdk-tools-linux-4333796.zip
-cp /usr/share/zoneinfo/Europe/Paris android/timezone/Paris
